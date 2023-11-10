1baa30016c6854ed4aaf79cf86b939f337c01f07
Thomas Rice <ricethomasj@gmail.com>
2023-11-10 16:26:29 -0600

    Added tags promises

---
+++ api/tags.js
@@ -0,0 +1,108 @@
+const endpoint = 'http://127.0.0.1:8000';
+
+const getAllTags = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/tags`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSingleTag = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/tags/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateTag = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/tags/${payload.id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteTag = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/tags/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const getPostTags = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/posttags`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSinglePostTag = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/posttags/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deletePostTag = (id) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/posttags/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getAllTags,
+  getPostTags,
+  getSinglePostTag,
+  getSingleTag,
+  deletePostTag,
+  updateTag,
+  deleteTag,
+};
